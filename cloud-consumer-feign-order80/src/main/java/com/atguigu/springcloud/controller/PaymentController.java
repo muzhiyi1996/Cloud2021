0b07748fff14fa6806e0db60940479055385b87b
muzhiyi <1156508380>
2021-08-18 21:47:15 +0800

    opemFeign服务调用

---
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,22 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.service.PaymentFeignService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class PaymentController {
+
+    @Resource
+    private PaymentFeignService paymentFeignService;
+
+    @GetMapping("/consumer/payment/get/{id}")
+    public CommonResult<Payment> get(@PathVariable Long id){
+        return paymentFeignService.get(id);
+    }
+}
